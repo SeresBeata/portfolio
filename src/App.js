2c8932f382a7fcbd6c1fd1aa30e4df851636fad7
MolnarBeata <115145107+SeresBeata@users.noreply.github.com>
2024-05-09 15:48:05 +0200

    Create dark mode at App.js
    Import ThemeProvider, createTheme, Box from MUI. Create and use darkTheme.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+//style
 import './App.css';
+//MUI
+import { Box, ThemeProvider, createTheme } from '@mui/material';
 
 const App = () => {
-  return <div>Hello World!</div>;
+  const darkTheme = createTheme({
+    palette: {
+      mode: 'dark',
+    },
+  });
+
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <Box bgcolor={'background.default'} color={'text.primary'}>
+      </Box>
+    </ThemeProvider>
+  );
 };
 
 export default App;
