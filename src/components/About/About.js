2f925e01d1ae4fe6fc8bc105890593c65a1c4141
MolnarBeata <115145107+SeresBeata@users.noreply.github.com>
2024-07-08 17:41:29 +0200

    Update About section for CVs
    Use ButtonGroup for en/de CVs.

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -6,6 +6,7 @@ import {
   Typography,
   Button,
   CardMedia,
+  ButtonGroup,
 } from '@mui/material';
 import PortraitIcon from '@mui/icons-material/Portrait';
 import './About.css';
@@ -13,7 +14,7 @@ import './About.css';
 const About = ({ lang }) => {
   return (
     <div className="about-container">
-      <img src="https://github.com/SeresBeata/achievement-1/blob/main/img/k%C3%A9p_%C3%B6n.png?raw=true" />
+      <img src={process.env.REACT_APP_IMG} />
       <Box>
         <h2>{lang.about_title}</h2>
         <Typography sx={{ mb: 1.5 }} color="text.secondary">
@@ -26,20 +27,40 @@ const About = ({ lang }) => {
           {lang.about_section_third}
         </Typography>
         <div className="btn-container">
-          <a
-            href="https://drive.google.com/file/d/1rho6hWbEen06itH1-KS9sw2lTOz-W4yD/view?usp=sharing"
-            target="_blank"
-            rel="noreferrer"
+          <ButtonGroup
+            variant="contained"
+            color="secondary"
+            aria-label="Basic button group"
           >
-            <Button
-              sx={{ marginTop: '10px' }}
-              variant="contained"
-              color="secondary"
-              endIcon={<PortraitIcon />}
+            <a
+              href={process.env.REACT_APP_CV_EN}
+              target="_blank"
+              rel="noreferrer"
             >
-              curriculum vitae
-            </Button>
-          </a>
+              <Button
+                sx={{ marginTop: '10px' }}
+                variant="contained"
+                color="secondary"
+                endIcon={<PortraitIcon />}
+              >
+                CV-EN
+              </Button>
+            </a>
+            <a
+              href={process.env.REACT_APP_CV_DE}
+              target="_blank"
+              rel="noreferrer"
+            >
+              <Button
+                sx={{ marginTop: '10px' }}
+                variant="contained"
+                color="secondary"
+                endIcon={<PortraitIcon />}
+              >
+                CV-DE
+              </Button>
+            </a>
+          </ButtonGroup>
         </div>
       </Box>
     </div>
